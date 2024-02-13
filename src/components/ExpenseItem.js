84e9a65fcddc30c3fbfc8baa266ca21350cb01b1
hrd05 <harshdunkhwal55@gmail.com>
2024-02-13 22:37:48 +0530

    passing data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,15 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+
     return (
-        <div>
-            <h2>Expense Items</h2>
-            <p>Food Rs 10</p>
-            <p>Petrol Rs 100</p>
-            <p>Movies Rs 200</p>
+        <div className="expense-item ">
+            <div>{props.date.toDateString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <p>{props.place}</p>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
         </div>
     );
 }
